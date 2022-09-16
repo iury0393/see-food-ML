942073e7c0a2eff4a6b4abe61b60fb018ecce652
Iury Vasconcelos <iury0393@gmail.com>
2022-09-16 09:48:47 -0300

    first commit

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
